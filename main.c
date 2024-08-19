a265effae290704e4fe84054243f4b670936e05a
Pedro Henrique da Silva Teixeira <pedrokk505@gmail.com>
2024-08-19 12:42:39 -0300

    Add book information functionality and display stored book details

--- main.c
+++ main.c
@@ -18,36 +18,52 @@ int main() {
   i = input = count = 0;
 
   while (input != 5) {
-    printf("\n\n============== WELCOME TO LIBRARY ==============");
+    printf("\n============== WELCOME TO LIBRARY ==============");
     printf("\n\n[1] Add book information\n[2] Display book information");
     printf("\n[3] List all book of given author\n[4] List the count of books in the library\n[5] Exit");
     printf("\n\nEnter one of the above: ");
     scanf("%d", &input);
+    getchar();
 
     switch (input) {
+    // Add book
     case 1:
+      printf("================================================\n");
       printf("Enter book name: ");
-      scanf(" %s", lib[i].bookName);
+      fgets(lib[i].bookName, sizeof(lib[i].bookName), stdin);
+      lib[i].bookName[strcspn(lib[i].bookName, "\n")] = 0;
 
       printf("Enter author name: ");
-      scanf(" %s", lib[i].author);
+      fgets(lib[i].author, sizeof(lib[i].author), stdin);
+      lib[i].author[strcspn(lib[i].author, "\n")] = 0;
 
       printf("Enter pages: ");
-      scanf(" %s", &lib[i].pages);
+      scanf(" %d", &lib[i].pages);
 
       printf("Enter price: ");
-      scanf(" %s", &lib[i].price);
+      scanf(" %f", &lib[i].price);
       count++;
+      i++;
       break;
     
+    // Print book information
     case 2:
-      
+      printf("================================================\n");
+      printf("you have entered the following information:");
+        for (i = 0; i < count; i++) {
+          printf("\n\nbook name = %s", lib[i].bookName);
+          printf("\t author name = %s", lib[i].author);
+          printf("\t  pages = %d", lib[i].pages);
+          printf("\t  price = %.2f\n", lib[i].price);
+        }
       break;
     
+    // Take the author name as input
     case 3:
       
       break;
     
+    // Print number of book in library
     case 4:
       
       break;
